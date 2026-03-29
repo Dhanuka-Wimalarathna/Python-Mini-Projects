796579309f1b9b0aa3d390b629344d5535ae18ac
Dhanuka Wimalarathna <dhanukawimalarathna2001@gmail.com>
2026-03-29 21:37:55 +0530

    Create the storage file.

---
+++ Expense_Tracker/storage.py
@@ -0,0 +1,21 @@
+# Handles reading/writing JSON file
+
+import os
+import json
+
+DATA_FILE = 'expenses.json'
+
+# Loading Expenses
+def load_expenses():
+    try:
+        with open (DATA_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return[]
+
+
+# Saving Expenses
+def save_expenses(expenses):
+    with open (DATA_FILE, 'w') as file:
+        json.dump(expenses, file, indent=4)
+        
---
+++ Expense_Tracker/summary.py
@@ -0,0 +1,1 @@
+# Handles summary calculations
