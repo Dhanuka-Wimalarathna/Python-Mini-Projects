6ae3b517535d2e655af254d0fb025bfcee119706
Dhanuka Wimalarathna <dhanukawimalarathna2001@gmail.com>
2026-03-29 22:08:28 +0530

    Create the function for show total summary.

--- Expense_Tracker/summary.py
+++ Expense_Tracker/summary.py
@@ -1,4 +1,14 @@
 # Handles summary calculations
 
-from expenses import list_expenses
+from storage import load_expenses
 
+# Total Summary
+def total_summary():
+    expenses = load_expenses()
+
+    if not expenses:
+        print("No expenses found!")
+        return
+
+    total = sum(e['amount'] for e in expenses)
+    print(f"Total Expenses: ${total}")
